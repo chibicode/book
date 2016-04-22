473bc3d0b0d4d933acd5c9157896cf076f0a6aa6
Shu Uesugi <shu@chibicode.com>
2016-04-21 18:57:19 -0700

    Initial layout setup

--- packages/deep-learning-junior-devs-book/src/index.js
+++ packages/deep-learning-junior-devs-book/src/index.js
@@ -1,17 +1,31 @@
-import React, { Component, PropTypes } from 'react'
+import React, { createElement, Component, PropTypes } from 'react'
 import './styles/index.css'
 
 export default class Book extends Component {
-  static contextTypes = {
+  static propTypes = {
+    components: PropTypes.object
+  };
+
+  static childContextTypes = {
     renderTutorialComponent: PropTypes.func
   };
 
+  getChildContext () {
+    return { renderTutorialComponent: ::this.renderTutorialComponent }
+  }
+
+  renderTutorialComponent (name, props) {
+    return createElement(this.props.components[name], props)
+  }
+
   render () {
     return (
-      <div>
-        <h2>Book?</h2>
+      <div style={{ padding: '50px 100px 100px' }}>
+        <div className='max-width-3'>
+          <h1 className='mt0'>Depth 2.1: Matrix Component</h1>
+        </div>
         <div>
-          { this.context.renderTutorialComponent('Test', { foo: 1 }) }
+          Hello World!
         </div>
       </div>
     )
--- src/App.js
+++ src/App.js
@@ -3,23 +3,9 @@ import Book from 'deep-learning-junior-devs-book'
 import components from './components'
 
 export default class App extends Component {
-  static childContextTypes = {
-    renderTutorialComponent: PropTypes.func
-  };
-
-  getChildContext () {
-    return {
-      renderTutorialComponent: (name, props) => createElement(components[name], props)
-    }
-  }
-
   render () {
     return (
-      <div>
-        <h1>Hello, world.</h1>
-        <Book />
-        <p>Goodbye.</p>
-      </div>
+      <Book { ...{ components }}/>
     )
   }
 }
