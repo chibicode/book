a8f68f0db154c0f8b5a47b6c85ef686a34859299
Shu Uesugi <shu@chibicode.com>
2016-04-24 00:26:43 -0700

    ComponentEmbed

--- packages/deep-learning-junior-devs-book/src/components/Depth1.js
+++ packages/deep-learning-junior-devs-book/src/components/Depth1.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router'
 import PageTitle from './PageTitle'
 import Section from './Section'
 import Snippet from './Snippet'
+import ComponentEmbed from './ComponentEmbed'
 import ComponentSnippet from './ComponentSnippet'
 import Depth1Matrix1 from './Depth1Matrix1'
 
@@ -16,18 +17,16 @@ const Depth1 = () =>
     </PageTitle>
     <Section id={1} />
     <Snippet id={1} />
-    <Section id={2} hint={true} />
+    <Section id={2} hint />
     <Section id={3} />
     <Snippet id={2} />
     <Section id={4} />
-    <div className='mb2'>
-      <Depth1Matrix1 data={
-        [
-          [1, 2, 3],
-          [4, 5, 6]
-        ]
-      } />
-    </div>
+    <ComponentEmbed>
+      <Depth1Matrix1 data={[
+        [1, 2, 3],
+        [4, 5, 6]
+      ]} />
+    </ComponentEmbed>
     <ComponentSnippet name='Depth1Matrix1' />
   </div>
 
