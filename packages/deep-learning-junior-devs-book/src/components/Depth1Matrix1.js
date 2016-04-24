c12795cfe79afa59d4db8cf776857c2ce518dc76
Shu Uesugi <shu@chibicode.com>
2016-04-23 20:32:04 -0700

    Testing out CSS modules for matrix

--- packages/deep-learning-junior-devs-book/src/components/Depth1Matrix1.js
+++ packages/deep-learning-junior-devs-book/src/components/Depth1Matrix1.js
@@ -1,15 +1,17 @@
 import React from 'react'
+// TODO: Export this at the top level so users can use it
+import styles from './styles/Matrix.css'
 
 const Depth1Matrix1 = ({ data }) =>
-  <table style={{ borderSpacing: 0, borderCollapse: 'collapse' }}>
+  <table>
     <tbody>
       {
         data.map((row, i) =>
-          <tr key={ i }>
+          <tr key={i}>
             {
               row.map((item, j) =>
-                <td className='py1 px2 border' key={ `${i},${j}` }>
-                  { item }
+                <td className={`py1 px2 ${styles.matrixCell}`} key={`${i},${j}`}>
+                  {item}
                 </td>)
             }
           </tr>
